78a81c4c0fe9cc3fedcc9d54f1e9db30ba653aad
nampc1618 <nampc1618@gmail.com>
2025-03-12 23:23:40 +0700

    add new file

---
+++ Models/SystemModel.cs
@@ -0,0 +1,15 @@
+﻿using DocumentFormat.OpenXml.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSS_HVCement.Models
+{
+    public class SystemModel
+    {
+        public string IP {  get; set; }
+        public int Port { get; set; }
+    }
+}
