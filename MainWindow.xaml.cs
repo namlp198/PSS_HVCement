3cd2f008706b93336fef538f83b0139f7c686ad1
nampc1618 <nampc1618@gmail.com>
2025-03-13 09:15:19 +0700

    add button for testing send data

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace PSS_HVCement
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool m_bFlag = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -32,6 +33,26 @@ namespace PSS_HVCement
             base.OnClosed(e);
 
             Application.Current.Shutdown();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!m_bFlag)
+            {
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    MainWindowViewModel.Instance.SendPrinterStatus("01", 1);
+                }));
+            }
+            else
+            {
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    MainWindowViewModel.Instance.SendPrinterStatus("01", 2);
+                }));
+            }
+
+            m_bFlag = !m_bFlag;
         }
     }
 }
